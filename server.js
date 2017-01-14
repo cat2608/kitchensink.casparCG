9cb97c9a9d02fceef8d3805acded6d201c757559
Catalina Oyaneder <cat2608@gmail.com>
2017-01-14 11:40:15 +0000

    Fix indentation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const http = require('http')
+const net = require('net')
 const path = require('path')
 const port = 4000
 
@@ -8,28 +9,28 @@ var connection = new CasparCG();
 
 
 const requestHandler = (request, response) => {
-	let body = '';
+  let body = '';
 
-	request.on('data', (chunk) => {
-		body += chunk;
-	});
+  request.on('data', (chunk) => {
+    body += chunk;
+  });
 
-	request.on('end', () => {
+  request.on('end', () => {
 
-		let data = body.toString('base64');
+    let data = body.toString('base64');
 
-		if (request.url.match(/update/)) {
-			connection.cgUpdate(1, 20, 1, data);
-			response.end(JSON.stringify({ msg: 'ok' }));
+    if (request.url.match(/update/)) {
+      connection.cgUpdate(1, 20, 1, data);
+      response.end(JSON.stringify({ msg: 'ok' }));
 
-		} else if (request.url.match(/start/)) {
-			connection.play(1, 10, data.title, true, 'SLIDE', 50, 'EaseInOutQuad', 'RIGHT');
+    } else if (request.url.match(/start/)) {
+      connection.play(1, 10, data.title, true, 'SLIDE', 50, 'EaseInOutQuad', 'RIGHT');
 
-		} else {
-			response.writeHead(200, { 'Content-Type': 'text/html' });
-			fs.createReadStream('index.html').pipe(response);
-		}
-	});
+    } else {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      fs.createReadStream('index.html').pipe(response);
+    }
+  });
 }
 
 const server = http.createServer(requestHandler)
